be847c5b7a1206a78736a36b80d63d4fb85d0009
KyleRamkissoon <61766735+KyleRamkissoon@users.noreply.github.com>
2020-04-23 06:45:59 -0400

    Add files via upload

---
+++ public/app.js
@@ -0,0 +1,116 @@
+
+const iconElement = document.querySelector(".weather-icon");
+const tempElement = document.querySelector(".temperature-value p");
+const descElement = document.querySelector(".temperature-description p");
+const locationElement = document.querySelector(".location p");
+const notificationElement = document.querySelector(".notification");
+var input = document.querySelector('.input_text');
+var button= document.querySelector('.submit');
+
+
+const weather = {};
+
+weather.temperature = {
+    unit : "celsius"
+}
+
+
+const KELVIN = 273; //for conversion
+
+const key = "82005d27a116c2880c8f0fcb866998a0"; //Api Key
+
+// Check to see if browser supports geolocation
+if('geolocation' in navigator){
+    navigator.geolocation.getCurrentPosition(setPosition, showError);
+}else{
+    notificationElement.style.display = "block";
+    notificationElement.innerHTML = "<p>Browser doesn't Support Geolocation</p>";
+}
+
+// Sets the user position
+function setPosition(position){
+    let latitude = position.coords.latitude;
+    let longitude = position.coords.longitude;
+
+    getWeather(latitude, longitude);
+}
+
+// shows an error when there is an issue with geolocation service
+function showError(error){
+    notificationElement.style.display = "block";
+    notificationElement.innerHTML = `<p> ${error.message} </p>`;
+}
+
+// Fuction to get weather info from api provider
+function getWeather(latitude, longitude){
+    let api = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${key}`;
+
+    fetch(api)
+        .then(function(response){
+            let data = response.json();
+            return data;
+        })
+        .then(function(data){
+            weather.temperature.value = Math.floor(data.main.temp - KELVIN);
+            weather.description = data.weather[0].description;
+            weather.iconId = data.weather[0].icon;
+            weather.city = data.name;
+            weather.country = data.sys.country;
+
+        })
+        .then(function(){
+            displayWeather();
+        });
+}
+
+// Display the weather stats to UI
+function displayWeather(){
+    iconElement.innerHTML = `<img src="icons/${weather.iconId}.png"/>`;
+    tempElement.innerHTML = `${weather.temperature.value}°<span>C</span>`;
+    descElement.innerHTML = weather.description;
+    locationElement.innerHTML = `${weather.city}, ${weather.country}`;
+}
+
+// C -> F conversion
+function celsiusToFahrenheit(temperature){
+    return (temperature * 9/5) + 32;
+}
+
+// Flip click from C -> F
+tempElement.addEventListener("click", function(){
+    if(weather.temperature.value === undefined) return;
+
+    if(weather.temperature.unit == "celsius"){
+        let fahrenheit = celsiusToFahrenheit(weather.temperature.value);
+        fahrenheit = Math.floor(fahrenheit);
+
+        tempElement.innerHTML = `${fahrenheit}°<span>F</span>`;
+        weather.temperature.unit = "fahrenheit";
+    }else{
+        tempElement.innerHTML = `${weather.temperature.value}°<span>C</span>`;
+        weather.temperature.unit = "celsius"
+    }
+
+});
+
+//Search Function
+button.addEventListener('click', function(name){
+fetch('https://api.openweathermap.org/data/2.5/weather?q='+input.value+'&appid=82005d27a116c2880c8f0fcb866998a0')
+.then(response => response.json())
+.then(data => {
+  weather.temperature.value = Math.floor(data.main.temp - KELVIN);
+  weather.description = data.weather[0].description;
+  weather.iconId = data.weather[0].icon;
+  weather.city = data.name;
+  weather.country = data.sys.country;
+
+
+  iconElement.innerHTML = `<img src="icons/${weather.iconId}.png"/>`;
+  tempElement.innerHTML = `${weather.temperature.value}°<span>C</span>`;
+  descElement.innerHTML = weather.description;
+  locationElement.innerHTML = `${weather.city}, ${weather.country}`;
+
+})
+
+.catch(err => alert("Incorrect Spelling or Entry"));
+})
